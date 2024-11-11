51736ff306fb16fdb0c3d27f1747c9cfcf0c0796
eminoz <eminoz@yahoo.com>
2024-11-11 22:51:40 +0300

    feat: :sparkles: vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut scores :Vec<u16> = vec![1, 2, 3, 4, 5];
+    scores.push(6);
+
+    for score in scores.iter() {
+        print!("{}, ", score);
+    }
+
+    let last = scores.pop();
+    println!("\nLast element: {:?}", last);
+
+    let mut color =Vec::new();
+    color.push(String::from("Red"));
+
+    let numbers =(8..=27).collect::<Vec<i32>>();
+    println!("{:?}", numbers);
+    let first_two = &numbers[0..2].to_vec();
+    println!("{:?}", first_two);
 }
