9943a32d0d9470f68e22ae91bdc6c33fb2972ce1
eminoz <eminoz@yahoo.com>
2024-11-20 12:25:04 +0300

    feat: add User struct and update user1 details

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        name: String::from("emin"),
+        suspended: false,
+        email: String::from("emin@emin.com"),
+    };
+    user1.suspended = true;
+    user1.email = String::from("emin2@e.com");
+    user1.name = String::from("emin2");
+}
+
+struct User {
+    name: String,
+    suspended: bool,
+    email: String,
 }
